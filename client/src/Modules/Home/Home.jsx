ccb4b8b3dc4e394297320bbec1ff782f742f989a
rusito23 <i.andruskiewitsch23@gmail.com>
2019-05-22 16:26:18 -0300

    Adding style to HomePage

--- client/src/Modules/Home/Home.jsx
+++ client/src/Modules/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Home.sass';
 import Login from './Login/Login.jsx'
 import delicious_pizza from '../../assets/img/delicious_pizza.jpg'
@@ -6,12 +6,15 @@ import delicious_pizza from '../../assets/img/delicious_pizza.jpg'
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        <div className="Home-img">
-          <img src={delicious_pizza} alt={"Delicious Pizza!"} />
+      <div className={"Home"}>
+        <div className={"Home-column Home-img"}>
+          <div className={"Home-title-cover"}>
+            <img src={delicious_pizza} alt={"Delicious Pizza!"}/>
+          </div>
+          <h1>Sagandunga!</h1>
         </div>
-        <div className="Home-login-register">
-          <Login />
+        <div className={"Home-column Home-login-register"}>
+          <Login/>
         </div>
       </div>
     );
